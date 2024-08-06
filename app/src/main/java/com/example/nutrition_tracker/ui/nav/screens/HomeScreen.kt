03fef89c197711f08faf2f2b56a225352ea21b01
vivek-vishwanath <vivs005@gmail.com>
2024-08-06 01:25:21 -0400

    Render Profile macros on HomeScreen

--- app/src/main/java/com/example/nutrition_tracker/ui/nav/screens/HomeScreen.kt
+++ app/src/main/java/com/example/nutrition_tracker/ui/nav/screens/HomeScreen.kt
@@ -1,33 +1,36 @@
 package com.example.nutrition_tracker.ui.nav.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.nutrition_tracker.data.Profile
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Welcome to Home Screen!", fontSize = 24.sp, textAlign = TextAlign.Center)
-        FloatingActionButton(onClick = { navController.navigate("add_meal") }, modifier = Modifier.align(Alignment.BottomEnd).padding(16.dp)) {
+        Column {
+            val macros = Profile.today.nutrition.macros
+            Text(text = "Carbs = ${macros.totalCarb}")
+            Text(text = "Fats = ${macros.totalFat}")
+            Text(text = "Protein = ${macros.protein}")
+            Text("Welcome to Home Screen!", fontSize = 24.sp, textAlign = TextAlign.Center)
+        }
+        FloatingActionButton(onClick = { navController.navigate("add_meal") }, modifier = Modifier
+            .align(Alignment.BottomEnd)
+            .padding(16.dp)) {
             Icon(Icons.Filled.Add, contentDescription = "Add")
         }
     }
